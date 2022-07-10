75f18d1bab29e88532039c954297683be0ff396a
Charlotte Quenardel <charlottequenardel@hotmail.com>
2022-07-10 15:52:08 +0200

    évolutions solution

--- src/App.js
+++ src/App.js
@@ -21,16 +21,17 @@ function App() {
   const [showDetail, setShowDetail] = useState(false);
   const [modalData, setModalData] = useState({});
   const [showResult, setShowResult] = useState(false);
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState(false);
   const [foundPairs, setFoundPairs] = useState([]); //On ne stockera que l'id de la dernière carte retournée
   const [bravo, setBravo] = useState(false);
   const [showFoundPairs, setShowFoundPairs] = useState(false);
+  const [imgBorder, setImgBorder] = useState("0px solid transparent");
 
   const target = useRef(null);
 
   //FONCTION MELANGE CARTES
   const shuffleCards = () => {
-    const shuffleCardListCopy = [...shuffleCardList].sort(
+    let shuffleCardListCopy = [...shuffleCardList].sort(
       () => Math.random() - 0.5
     );
     setShuffleCardList(shuffleCardListCopy);
@@ -70,7 +71,7 @@ function App() {
 
   //BANDEAU SUPERIEUR
   let header = (
-    <Row className="row title">
+    <div className="row header">
       <h1>LE MEMORY DU JARDINIER</h1>
       <button className="shufflebtn" onClick={newGame}>
         Nouvelle partie
@@ -81,30 +82,33 @@ function App() {
       >
         {phrase}
       </button>
-    </Row>
+    </div>
   );
 
   //FONCTION CLIC SUR CARTE
   const cardFlip = (cardId) => {
     //Récupération de l'item de la liste d'image
-    let card = shuffleCardList.find((e) => e.id == cardId);
+    let card = shuffleCardList.find((e) => e.id === cardId);
     //1. changer item.isVisible dans shuffleCardList
     let cardListCopy = [...shuffleCardList];
     card.isVisible = !card.isVisible;
     setShuffleCardList(cardListCopy);
 
     //2. s'il s'agit de la deuxième carte retournée : vérifier si la paire est bonne
-    let displayedCards = shuffleCardList.filter((e) => e.isVisible == true);
+    let displayedCards = shuffleCardList.filter((e) => e.isVisible === true);
     if (displayedCards.length > 1) {
-      if (displayedCards.find((e) => e.id == card.pair)) {
-        console.log("paire trouvée");
-        setResult("OK");
+      if (displayedCards.find((e) => e.id === card.pair)) {
+        console.log("le résultat est OK");
+        setImgBorder("2px solid green");
+        console.log("les trucs sont verts");
         setFoundPairs([...foundPairs, cardId]);
-        setShowResult(true);
+        setTimeout(() => next(true), 2500);
+        // setShowResult(true);
       } else {
-        setResult("KO");
-        setShowResult(true);
+        setImgBorder("2px solid red");
+        // setShowResult(true);
         console.log("raté");
+        setTimeout(() => next(false), 2500);
       }
     }
   };
@@ -116,19 +120,25 @@ function App() {
   };
 
   //SET DU TEXTE DE LA MODAL SI 2 CARTES RETOURNEES
-  let resultToDisplay = "";
-  if (result == "OK") {
-    resultToDisplay = "Bravo ! ";
-  } else resultToDisplay = "Raté ! Essayez encore !";
+  // let resultToDisplay = "";
+  // if (result == "OK") {
+  //   resultToDisplay = "Bravo ! ";
+  // } else resultToDisplay = "Raté ! Essayez encore !";
 
   //FONCTION CLIC SUR "CONTINUER" DE LA POPUP RESULTAT
-  const next = (givenResult) => {
+  //FONCTION DE CALLBACK SETTIMEOUT
+  const next = (bool) => {
+    console.log("Next se lance");
+    console.log("le resultat est", result);
     let cardListCopy = [...shuffleCardList];
     for (let element of cardListCopy) {
-      if (element.isVisible == true) {
-        if (givenResult == "OK") {
-          element.isFound = !element.isFound;
+      if (element.isVisible === true) {
+        if (bool) {
+          console.log("je suis dans le résultat OK");
+          element.isFound = true;
+          console.log("j'ai mis à jour le isFOundàtrue");
           setFoundPairNb(foundPairNb + 1);
+          console.log("j'ai ajouté le foundPairNbs");
           if (!shuffleCardList.some((e) => !e.isFound)) {
             setBravo(true);
           }
@@ -137,33 +147,36 @@ function App() {
       }
     }
     setShuffleCardList(cardListCopy);
-    setShowResult(!showResult);
+    setImgBorder("0px solid transparent");
+    setResult("");
+    // setShowResult(!showResult);
   };
 
   //SETUP DE VARIABLES QUI PEUVENT ETRE UNDEFINED
   let binomName;
   let binomAuthor;
   if (modalData.pair) {
-    let binom = shuffleCardList.find((e) => e.id == modalData.pair);
+    let binom = shuffleCardList.find((e) => e.id === modalData.pair);
     binomName = binom.name;
     binomAuthor = binom.author;
   }
 
   let imageSource;
   if (modalData.url) {
-    imageSource = shuffleCardList.find((e) => e.id == modalData.pair).url;
+    imageSource = shuffleCardList.find((e) => e.id === modalData.pair).url;
   }
 
   //LISTE DE CARTES A AFFICHER
   let cardItems = shuffleCardList.map((item, i) => {
     if (item.isFound) {
-      return <Col className="card" key={i}></Col>;
+      return <Col className="card" key={i} alt={item.author}></Col>;
     } else if (!item.isVisible) {
       return (
         <Col className="card" key={i} onClick={() => cardFlip(item.id)}>
           <img
             src="/images/garden.jpg"
             className="Image"
+            alt={item.author}
             style={{ opacity: 0.9 }}
           ></img>
         </Col>
@@ -174,6 +187,8 @@ function App() {
           <img
             src={item.url}
             className="Image"
+            alt={item.author}
+            style={{ border: imgBorder }}
             onClick={() => cardFlip(item.id)}
           ></img>
           <QuestionCircleFilled
@@ -191,23 +206,26 @@ function App() {
     return (
       <div className="Result list" key={i}>
         <div>
+          <p>{shuffleCardList.find((e) => e.id === id).name}</p>
           <img
             className="imagePairs"
-            src={shuffleCardList.find((e) => e.id == id).url}
+            alt={shuffleCardList.find((e) => e.id === id).author}
+            src={shuffleCardList.find((e) => e.id === id).url}
           />
-          <p>{shuffleCardList.find((e) => e.id == id).name}</p>
           <p className="author">
-            {shuffleCardList.find((e) => e.id == id).author}
+            {shuffleCardList.find((e) => e.id === id).author}
           </p>
         </div>
         <div>
+          <p>{shuffleCardList.find((e) => e.pair === id).name}</p>
           <img
             className="imagePairs"
-            src={shuffleCardList.find((e) => e.pair == id).url}
+            alt={shuffleCardList.find((e) => e.pair === id).author}
+            src={shuffleCardList.find((e) => e.pair === id).url}
           />
-          <p>{shuffleCardList.find((e) => e.pair == id).name}</p>
+
           <p className="author">
-            {shuffleCardList.find((e) => e.pair == id).author}
+            {shuffleCardList.find((e) => e.pair === id).author}
           </p>
         </div>
       </div>
@@ -215,67 +233,73 @@ function App() {
   });
 
   return (
-    <Container className="container">
+    <div>
       {header}
-      <Row sm="5">{cardItems}</Row>
-      <Modal
-        size="lg"
-        style={{ maxWidth: "900px", width: "100%" }}
-        isOpen={showDetail}
-        toggle={() => setShowDetail(!showDetail)}
-      >
-        <ModalHeader toggle={() => setShowDetail(!showDetail)}>
-          Je suis {modalData.name}
-        </ModalHeader>
-        <ModalBody className="detailModal">
-          <div className="modalColumn">
-            <img className="ImageModal" src={modalData.url} />
-            <p className="author">{modalData.author}</p>
-            <p>{modalData.detail}</p>
-          </div>
-          <div className="modalColumn">
-            <img className="ImageModal" src={imageSource} />
-            <p className="author">{binomAuthor}</p>
-            <p>Mon binôme est {binomName}</p>
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <Button onClick={() => setShowDetail(!showDetail)}>
-            J'ai compris !
-          </Button>
-        </ModalFooter>
-      </Modal>
-      <Modal isOpen={showResult} toggle={() => next(result)}>
-        <ModalBody className="Result">
-          <div>{resultToDisplay}</div>
-          <Button onClick={() => next(result)}>Continuer</Button>
-        </ModalBody>
-      </Modal>
-      <Modal isOpen={bravo} toggle={newGame}>
-        <ModalBody className="Result">
-          <div>Gagné! Bravo ! </div>
-          <Button onClick={newGame}>Nouvelle partie</Button>
-        </ModalBody>
-      </Modal>
-      <Modal
-        className="Result"
-        isOpen={showFoundPairs}
-        toggle={() => {
-          setShowFoundPairs(!showFoundPairs);
-        }}
-      >
-        <ModalHeader toggle={() => setShowFoundPairs(!showFoundPairs)}>
-          {phrase}
-        </ModalHeader>
-        <ModalBody>{foundPairsToShow}</ModalBody>
-        <ModalFooter>
-          {" "}
-          <Button onClick={() => setShowFoundPairs(!showFoundPairs)}>
-            J'ai compris !
-          </Button>
-        </ModalFooter>
-      </Modal>
-    </Container>
+      <Container className="container">
+        <Row xs="5">{cardItems}</Row>
+        <Modal
+          size="lg"
+          style={{ maxWidth: "900px", width: "100%" }}
+          isOpen={showDetail}
+          toggle={() => setShowDetail(!showDetail)}
+        >
+          <ModalHeader toggle={() => setShowDetail(!showDetail)}>
+            Je suis {modalData.name}
+          </ModalHeader>
+          <ModalBody className="detailModal">
+            <div className="modalColumn">
+              <img
+                className="ImageModal"
+                src={modalData.url}
+                alt={modalData.author}
+              />
+              <p className="author">{modalData.author}</p>
+              <p>{modalData.detail}</p>
+            </div>
+            <div className="modalColumn">
+              <img className="ImageModal" src={imageSource} alt={binomAuthor} />
+              <p className="author">{binomAuthor}</p>
+              <p>Mon binôme est {binomName}</p>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={() => setShowDetail(!showDetail)}>
+              J'ai compris !
+            </Button>
+          </ModalFooter>
+        </Modal>
+        {/* <Modal isOpen={showResult} toggle={() => next(result)}>
+          <ModalBody className="Result">
+            <div>{resultToDisplay}</div>
+            <Button onClick={() => next(result)}>Continuer</Button>
+          </ModalBody>
+        </Modal> */}
+        <Modal isOpen={bravo} toggle={newGame}>
+          <ModalBody className="Result">
+            <div>Gagné! Bravo ! </div>
+            <Button onClick={newGame}>Nouvelle partie</Button>
+          </ModalBody>
+        </Modal>
+        <Modal
+          className="Result"
+          isOpen={showFoundPairs}
+          toggle={() => {
+            setShowFoundPairs(!showFoundPairs);
+          }}
+        >
+          <ModalHeader toggle={() => setShowFoundPairs(!showFoundPairs)}>
+            {phrase}
+          </ModalHeader>
+          <ModalBody>{foundPairsToShow}</ModalBody>
+          <ModalFooter>
+            {" "}
+            <Button onClick={() => setShowFoundPairs(!showFoundPairs)}>
+              J'ai compris !
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </Container>
+    </div>
   );
 }
 
